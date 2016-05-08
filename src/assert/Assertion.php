7e5398ebbdad13f4904be90e9e29fd050d3ea32c
George Cooksey <texdc3@gmail.com>
2016-05-08 08:54:03 -0500

    Add assertions

---
+++ src/assert/Assertion.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * Assertion.php
+ *
+ * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @copyright 2016 George D. Cooksey, III
+ */
+
+namespace texdc\totp\assert;
+
+use Assert\Assertion as BaseAssertion;
+
+/**
+ * Adds missing validations
+ *
+ * @method static void isModulus($value, $modulo, $message = null, $propertyPath = null)
+ * @method static void notModulus($value, $modulo, $message = null, $propertyPath = null)
+ * @method static void integerRange($value, $min, $max, $message = null, $propertyPath = null)
+ * @method static void notIntegerRange($value, $min, $max, $message = null, $propertyPath = null)
+ * @method static void notContaints($value, $needle, $message = null, $propertyPath = null)
+ */
+class Assertion extends BaseAssertion
+{
+    const INVALID_MODULUS       = 400;
+    const INVALID_NUMERIC_RANGE = 401;
+
+    /**
+     * Assert that a value is a modulus of $modulo
+     *
+     * @param  numeric     $value
+     * @param  numeric     $modulo
+     * @param  string|null $message
+     * @param  string|null $propertyPath
+     * @return void
+     * @throws \Assert\AssertionFailedException
+     */
+    public static function isModulus($value, $modulo, $message = null, $propertyPath = null)
+    {
+        static::numeric($value);
+        static::numeric($modulo);
+
+        if ($value % $modulo !== 0) {
+            $message = sprintf(
+                $message ?: 'Value %d is not a modulus of %d',
+                static::stringify($value),
+                static::stringify($modulo)
+            );
+
+            throw static::createException($value, $message, static::INVALID_MODULUS, $propertyPath);
+        }
+    }
+
+    /**
+     * Assert that a value is not a modulus of $modulo
+     *
+     * @param  numeric     $value
+     * @param  numeric     $modulo
+     * @param  string|null $message
+     * @param  string|null $propertyPath
+     * @return void
+     * @throws \Assert\AssertionFailedException
+     */
+    public static function notModulus($value, $modulo, $message = null, $propertyPath = null)
+    {
+        static::numeric($value);
+        static::numeric($modulo);
+
+        if ($value % $modulo === 0) {
+            $message = sprintf(
+                $message ?: 'Value %d is a modulus of %d',
+                static::stringify($value),
+                static::stringify($modulo)
+            );
+
+            throw static::createException($value, $message, static::INVALID_MODULUS, $propertyPath);
+        }
+    }
+
+    /**
+     * Assert that a value is between $min and $max
+     *
+     * @param  numeric     $value
+     * @param  numeric     $min
+     * @param  numeric     $max
+     * @param  string|null $message
+     * @param  string|null $propertyPath
+     * @return void
+     * @throws \Assert\AssertionFailedException
+     */
+    public static function numericRange($value, $min, $max, $message = null, $propertyPath = null)
+    {
+        static::numeric($value);
+        static::numeric($min);
+        static::numeric($max);
+
+        if ($value < $min || $value > $max) {
+            $message = sprintf(
+                $message ?: 'Value %d is not between %d and %d',
+                static::stringify($value),
+                static::stringify($min),
+                static::stringify($max)
+            );
+
+            throw static::createException($value, $message, static::INVALID_NUMERIC_RANGE, $propertyPath);
+        }
+    }
+
+    /**
+     * Assert that a value is not between $min and $max
+     *
+     * @param  numeric     $value
+     * @param  numeric     $min
+     * @param  numeric     $max
+     * @param  string|null $message
+     * @param  string|null $propertyPath
+     * @return void
+     * @throws \Assert\AssertionFailedException
+     */
+    public static function notNumericRange($value, $min, $max, $message = null, $propertyPath = null)
+    {
+        static::numeric($value);
+        static::numeric($min);
+        static::numeric($max);
+
+        if ($value >= $min && $value <= $max) {
+            $message = sprintf(
+                $message ?: 'Value %d is between %d and %d',
+                static::stringify($value),
+                static::stringify($min),
+                static::stringify($max)
+            );
+
+            throw static::createException($value, $message, static::INVALID_NUMERIC_RANGE, $propertyPath);
+        }
+    }
+
+    /**
+     * Assert that a string does not contain $needle
+     *
+     * @param  string      $value
+     * @param  string      $needle
+     * @param  string|null $message
+     * @param  string|null $propertyPath
+     * @return void
+     * @throws \Assert\AssertionFailedException
+     */
+    public static function notContains($value, $needle, $message = null, $propertyPath = null)
+    {
+        static::string($value);
+        static::string($needle);
+
+        if (mb_strpos($value, $needle) !== false) {
+            $message = sprintf(
+                $message ?: 'Value "%s" contains "%s"',
+                static::stringify($value),
+                static::stringify($needle)
+            );
+
+            throw static::createException($value, $message, static::INVALID_STRING_CONTAINS, $propertyPath);
+        }
+    }
+}
---
+++ test/assert/AssertionTest.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * AssertionTest.php
+ *
+ * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @copyright 2016 George D. Cooksey, III
+ */
+
+namespace texdc\totp\test\assert;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use texdc\totp\assert\Assertion;
+
+class AssertionTest extends TestCase
+{
+    public function testExtendsAssertion()
+    {
+        $assert = new Assertion();
+        $this->assertInstanceOf('Assert\\Assertion', $assert);
+    }
+
+    public function testValidModulus()
+    {
+        Assertion::isModulus(24, 8);
+        Assertion::isModulus(9, 3);
+    }
+
+    public function testInvalidModulus()
+    {
+        $this->expectException('Assert\AssertionFailedException');
+        $this->expectExceptionCode(Assertion::INVALID_MODULUS);
+        $this->expectExceptionMessage('Value 15 is not a modulus of 4');
+        Assertion::isModulus(15, 4);
+    }
+
+    public function testValidNotModulus()
+    {
+        Assertion::notModulus(5, 3);
+        Assertion::notModulus(45, 17);
+    }
+
+    public function testInvalidNotModulus()
+    {
+        $this->expectException('Assert\AssertionFailedException');
+        $this->expectExceptionCode(Assertion::INVALID_MODULUS);
+        $this->expectExceptionMessage('Value 12 is a modulus of 4');
+        Assertion::notModulus(12, 4);
+    }
+
+    public function testValidNumericRange()
+    {
+        Assertion::numericRange(6, 6, 8);
+        Assertion::numericRange(45, 40, 50);
+    }
+
+    public function testInvalidNumericRange()
+    {
+        $this->expectException('Assert\AssertionFailedException');
+        $this->expectExceptionCode(Assertion::INVALID_NUMERIC_RANGE);
+        $this->expectExceptionMessage('Value 12 is not between 15 and 20');
+        Assertion::numericRange(12, 15, 20);
+    }
+
+    public function testValidNotNumericRange()
+    {
+        Assertion::notNumericRange(5, 6, 8);
+        Assertion::notNumericRange(55, 40, 50);
+    }
+
+    public function testInvalidNotNumericRange()
+    {
+        $this->expectException('Assert\AssertionFailedException');
+        $this->expectExceptionCode(Assertion::INVALID_NUMERIC_RANGE);
+        $this->expectExceptionMessage('Value 18 is between 15 and 20');
+        Assertion::notNumericRange(18, 15, 20);
+    }
+
+    public function testValidNotContains()
+    {
+        Assertion::notContains('foo', ':');
+        Assertion::notContains('bar', ';');
+    }
+
+    public function testInvalidNotContains()
+    {
+        $this->expectException('Assert\AssertionFailedException');
+        $this->expectExceptionCode(Assertion::INVALID_STRING_CONTAINS);
+        $this->expectExceptionMessage('Value ":test" contains ":"');
+        Assertion::notContains(':test', ':');
+    }
+}
